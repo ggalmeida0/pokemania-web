c78545cd82003770c371a6bb489ea821d271efcc
Gabriel <ggalmeida0@gmail.com>
2020-07-02 17:18:54 -0400

    Updates

---
+++ precache-manifest.acd6a9ceda7b5e0b99ca9add58f64145.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05694639cc36b9246650f543ae257d2e",
+    "url": "/pokemania/index.html"
+  },
+  {
+    "revision": "895c71013092369e312a",
+    "url": "/pokemania/static/css/main.6bdb6662.chunk.css"
+  },
+  {
+    "revision": "968b23427682990cd9e6",
+    "url": "/pokemania/static/js/2.c05a5776.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokemania/static/js/2.c05a5776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895c71013092369e312a",
+    "url": "/pokemania/static/js/main.4d1b9e04.chunk.js"
+  },
+  {
+    "revision": "d7fc834f26ed38e5318b",
+    "url": "/pokemania/static/js/runtime-main.44e18ecd.js"
+  },
+  {
+    "revision": "c136471d890ef25fc2c55c39af001aa8",
+    "url": "/pokemania/static/media/logo.c136471d.png"
+  }
+]);
